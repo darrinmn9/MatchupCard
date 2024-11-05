091d792f5cf94a5f55a0fc4d68062aa6e3cc3511
Darrin Nagengast <darrinlizz@Darrins-MacBook-Air.local>
2024-11-05 11:43:14 -0600

    finish circular progress svg, add documentation

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # MatchupCard Component
 
+## Project Folder Structure
+
+This projected was scaffolded with [create-vue](https://github.com/vuejs/create-vue) which provides a minimal project structure for a `Vue` + `Vite` + `Vitest` (unit testing) + `Eslint/Prettier` (code formatting/linting). This scaffold does install extra dependencies for the dev environment, but the overall production code is just `HTML` + `CSS` + `JS` with `VueJS` being the only production external dependency.
+
+
 ## Project Setup
 
 ```sh
@@ -17,3 +22,16 @@ npm run dev
 ```sh
 npm run test:unit
 ```
+
+## Example Component Usage
+```html
+<MatchupCard :matchups="matchupsList" />
+```
+
+## Known Limitations
+
+- Font, sizes and colors may not be exact, but a good approximation based off the image provided.
+- For the "ProgressCircular" component, I referenced an [open source example](https://vuetifyjs.com/en/components/progress-circular/) because I'm not a svg expert and was time constrained.
+- I am relying on the hardcoded `matchupsList` data always existing and did not handle empty/error/loading states.
+- The component always defaults to showing the 1st matchup, but could be improved to allow specifying an initial matchup index.
+- The component does not watch for mutations to the `matchups` array after initial render.
--- src/components/ProgressCircular.vue
+++ src/components/ProgressCircular.vue
@@ -1,4 +1,15 @@
 <script>
+/*
+ * based on https://vuetifyjs.com/en/components/progress-circular/
+ * https://github.com/vuetifyjs/vuetify/tree/master/packages/vuetify/src/components/VProgressCircular
+ */
+const RADIUS = 20
+const CIRCUMFERENCE = 2 * Math.PI * RADIUS
+const WIDTH = 6
+const SIZE = 100
+const DIAMETER = (RADIUS / (1 - WIDTH / SIZE)) * 2
+const STROKE_WIDTH = (WIDTH / SIZE) * DIAMETER
+
 export default {
   props: {
     value: {
@@ -9,7 +20,23 @@ export default {
       }
     }
   },
+  data() {
+    return {
+      RADIUS,
+      CIRCUMFERENCE,
+      WIDTH,
+      SIZE,
+      DIAMETER,
+      STROKE_WIDTH
+    }
+  },
   computed: {
+    strokeDashOffset() {
+      return `${((100 - this.value) / 100) * this.CIRCUMFERENCE}px`
+    },
+    isHalfComplete() {
+      return this.value >= 50
+    },
     percentageDisplay() {
       return `${this.value.toFixed(0)}%`
     }
@@ -18,7 +45,92 @@ export default {
 </script>
 
 <template>
-  <div>{{ percentageDisplay }}</div>
+  <div class="progress-circular">
+    <svg xmlns="http://www.w3.org/2000/svg" :viewBox="`0 0 ${DIAMETER} ${DIAMETER}`">
+      <circle
+        class="progress-circular-underlay"
+        fill="transparent"
+        cx="50%"
+        cy="50%"
+        :r="RADIUS"
+        :stroke-width="STROKE_WIDTH"
+        :stroke-dasharray="CIRCUMFERENCE"
+        stroke-dashoffset="0"
+      />
+      <circle
+        :class="
+          isHalfComplete
+            ? 'progress-circular-favorite-overlay'
+            : 'progress-circular-underdog-overlay'
+        "
+        fill="transparent"
+        cx="50%"
+        cy="50%"
+        :r="RADIUS"
+        :stroke-width="STROKE_WIDTH"
+        :stroke-dasharray="CIRCUMFERENCE"
+        :stroke-dashoffset="strokeDashOffset"
+        stroke-linecap="round"
+      />
+    </svg>
+    <div
+      :style="{
+        color: isHalfComplete ? 'green' : 'rgba(0, 0, 0, 0.36)',
+        'font-weight': isHalfComplete ? 'bold' : 'normal'
+      }"
+      class="progress-circular-content"
+    >
+      {{ percentageDisplay }}
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.progress-circular {
+  width: 70px;
+  height: 70px;
+  align-items: center;
+  display: inline-flex;
+  justify-content: center;
+  position: relative;
+  vertical-align: middle;
+}
+
+.progress-circular > svg {
+  width: 100%;
+  height: 100%;
+  margin: auto;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 0;
+  transform: rotate(calc(270deg));
+}
+
+.progress-circular-underlay {
+  color: rgba(0, 0, 0, 0.12);
+  stroke: rgba(0, 0, 0, 0.12);
+  z-index: 1;
+}
+
+.progress-circular-underdog-overlay {
+  color: rgba(0, 0, 0, 0.36);
+  stroke: rgba(0, 0, 0, 0.36);
+  z-index: 2;
+}
+
+.progress-circular-favorite-overlay {
+  color: green;
+  stroke: green;
+  z-index: 2;
+}
+
+.progress-circular-content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.5rem;
+}
+</style>
